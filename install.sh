57a9622e31610f0328b73d65edc3d3790667b41a
Eric Goddard <egoddard1010@gmail.com>
2016-11-11 14:14:32 -0600

    Fix typo in keyserver key value.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Install the required packages
 apt-get update
 apt-get install -y git nginx
 
 apt-get install -y apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 # Install docker and its dependencies
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee /etc/apt/sources.list.d/docker.list

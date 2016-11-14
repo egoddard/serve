60d8c4c1e9934804c7e9db42dad3c51525928aa1
Eric Goddard <egoddard@memphis.edu>
2016-11-14 16:37:30 -0600

    Update install.sh to include additional packages and make additional directories.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Install the required packages
 apt-get update
-apt-get install -y git nginx python-pip python-virtualenv
+apt-get install -y git nginx python-pip python-virtualenv pcregrep
 
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -19,21 +19,62 @@ service docker start
 
 useradd -m -G docker serve
 
+# Make folder to hold app nginx configs
+if [ ! -d '/etc/nginx/app.d' ]; then
+    mkdir /etc/nginx/app.d
+    chown serve:serve /etc/nginx/app.d
+fi
+
+# Make folder to hold app repos
+if [ ! -d '/opt/serve' ]; then
+    mkdir /opt/serve
+    chown serve:serve /opt/serve
+fi
+
+# Make sure ssh folder and authorized keys exist
+mkdir -p /home/serve/.ssh
+
+if [ ! -e /home/serve/.ssh/authorized_keys ]; then
+    touch /home/serve/.ssh/authorized_keys
+fi
+
+chown -R serve:serve /home/serve/.ssh
+chmod 700 /home/serve/.ssh
+
+# Delete existing installation
+if [ -d '/home/serve/serve' ]; then
+    echo "Removing existing installation..."
+    sudo -u serve -H sh -c "rm -rf /home/serve/serve/"
+fi
+
 # Copy the serve folder to the serve users's home directory and fix permissions
 echo "Copying the Serve app to the serve user's folder..."
+mkdir -p /home/serve/serve
 cp -r . /home/serve/serve
 chown -R serve:serve /home/serve/serve
+
 
 # Create a virtualenv to install the commands into
 echo "Creating the Python virtual environment..."
 sudo -u serve -H sh -c "virtualenv /home/serve/serve/.venv"
 
 echo "Activating the virtual environment..."
+echo `pwd`
+echo `ls`
 sudo -u serve -H sh -c "cd /home/serve/serve; . .venv/bin/activate; python setup.py install"
 
 echo "Adding serve command to path..."
 sudo -u serve -H sh -c "mkdir -p /home/serve/bin"
-sudo -u serve -H sh -c "ln -s /home/serve/serve/.venv/bin/serve /home/serve/bin/serve"
+
+if [ ! -h '/home/serve/bin/serve' ]; then
+    sudo -u serve -H sh -c "ln -s /home/serve/serve/.venv/bin/serve /home/serve/bin/serve"
+fi
+
+# Add serve to sudoers with permission to reload nginx
+echo "serve ALL = NOPASSWD: /etc/init.d/nginx reload,/etc/init.d/nginx start, /etc/init.d/nginx stop" >> /etc/sudoers
+
+# Replace default nginx configuration
+cp serve_nginx_default.conf /etc/nginx/sites-available/default
 
 echo "Finished installing Serve."
 echo "Restart the server to make sure the serve user has the correct permissions."
